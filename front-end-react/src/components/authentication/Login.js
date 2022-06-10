2f475ce58efa464508e346ba2ab317f4de8059f8
Robel <robelalemu2372@gmail.com>
2022-06-10 18:14:11 +0300

    modified login style

--- front-end-react/src/components/authentication/Login.js
+++ front-end-react/src/components/authentication/Login.js
@@ -4,6 +4,9 @@ import { useAuth } from "../../AuthContext/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 import AuthLayout from "../layout/AuthLayout";
 
+
+import Collapse from 'react-bootstrap/Collapse';
+
 export default function Login() {
   const emailRef = useRef();
   const passwordRef = useRef();
@@ -12,6 +15,8 @@ export default function Login() {
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 const  [loggedIn,setLoggedIn]= useState(false);
+
+const [open, setOpen] = useState(false);
   async function handleSubmit(e) {
     e.preventDefault();
     
@@ -76,17 +81,22 @@ const  [loggedIn,setLoggedIn]= useState(false);
   return (
     <>
       <AuthLayout>
-        <Card>
+        <Card >
           <Card.Body>
-            <h2 className="text-center mb-4">Log In</h2>
+            <h2 className="text-center mb-4"> <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-file-lock2-fill" viewBox="0 0 16 16">
+  <path d="M7 6a1 1 0 0 1 2 0v1H7V6z"/>
+  <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm-2 6v1.076c.54.166 1 .597 1 1.224v2.4c0 .816-.781 1.3-1.5 1.3h-3c-.719 0-1.5-.484-1.5-1.3V8.3c0-.627.46-1.058 1-1.224V6a2 2 0 1 1 4 0z"/>
+</svg> Sign In</h2>
             {error && <Alert variant="danger">{error}</Alert>}
+
+
             <Form onSubmit={handleSubmit}>
-              <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
+              <Form.Group  id="email"> 
+                <Form.Label><strong>Email</strong></Form.Label>
                 <Form.Control type="email" ref={emailRef} required />
               </Form.Group>
               <Form.Group id="password">
-                <Form.Label>Password</Form.Label>
+                <Form.Label><strong>Password</strong></Form.Label>
                 <Form.Control type="password" ref={passwordRef} required />
               </Form.Group>
               <Button disabled={loading} className="w-100" type="submit">
@@ -96,8 +106,45 @@ const  [loggedIn,setLoggedIn]= useState(false);
             {/* <div className="w-100 text-center mt-3">
               <Link to="/forgot-password">Forgot Password?</Link>
             </div> */}
+            
           </Card.Body>
+          
         </Card>
+        <br/>
+
+        <Button className="w-100" variant="outline-primary"
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+      >
+       <strong>Please click here to get Login information</strong> 
+      </Button>
+     
+      <Collapse in={open}>
+      
+        <div id="example-collapse-text">
+        <br/>
+        <Alert variant="info">
+    
+  <h5>Admin Login</h5>
+  <hr />
+  
+  <p className="mb-0">
+  <strong>Email:</strong>  abe@gmail.com<br/>
+   <strong>Password:</strong> abebe2372
+  </p>
+  <hr />
+  <h5>Data Encoder Login</h5>
+  <hr />
+  <p className="mb-0">
+  <strong>Email:</strong>  newadataE@gmail.com<br/>
+  <strong> Password:</strong> 111111
+  </p>
+</Alert>
+        </div>
+      </Collapse>
+
+        
         {/* <div className="w-100 text-center mt-2">
           Need an account? <Link to="/signup">Sign Up</Link>
         </div> */}
--- front-end-react/src/components/layout/AuthLayout.js
+++ front-end-react/src/components/layout/AuthLayout.js
@@ -7,7 +7,7 @@ function AuthLayout(props){
       className="d-flex align-items-center justify-content-center"
       style={{ minHeight: "100vh" }}
     >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
+      <div className="w-100" style={{ maxWidth: "600px" }}>
       {props.children}
       </div>
     </Container>
